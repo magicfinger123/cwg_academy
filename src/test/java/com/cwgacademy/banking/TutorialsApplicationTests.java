cd01fd5f9df554ce96464f46b6f3104b95976e66
Magicfinger <ossaimike8@gmail.com>
2022-03-11 16:05:03 +0100

    project was initialised

---
+++ src/test/java/com/cwgacademy/banking/TutorialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cwgacademy.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
